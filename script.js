1f8aa0247ea82ab3fb836a19c2ddf4bfcd854058
Meghan <nektromc@gmail.com>
2018-11-09 18:46:37 -0500

    add basic article loading

--- script.js
+++ script.js
@@ -14,3 +14,31 @@ if (!(here.searchParams.has("url"))) {
         location.assign(`./?url=${ele.children[2].children[1].value}`);
     });
 }
+else {
+    //
+    console.log("load article");
+    const ele = document.getElementById("section-article");
+    ele.removeAttribute("hidden");
+    const url = new URL("https://"+here.searchParams.get("url"));
+    //
+    Promise.resolve(PROXY + url.href)
+    .then(x => fetch(x))
+    .then(x => x.text())
+    .then(x => parse_html(x))
+    .then(x => load_article(ele, url, x))
+}
+
+//
+//
+function parse_html(string) {
+    const element = document.createElement("html");
+    element.innerHTML = string;
+    return element;
+}
+function load_article(ele, url, html) {
+    const domain = url.hostname;
+    ele.children[0].remove();
+    
+    switch (domain) {
+    }
+}
