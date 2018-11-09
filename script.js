f49b398e190fe8a9b9a0ee87887870e30adfb4cc
Meghan <nektromc@gmail.com>
2018-11-09 18:41:44 -0500

    add article submit input

--- script.js
+++ script.js
@@ -10,4 +10,7 @@ if (!(here.searchParams.has("url"))) {
     console.log("load index");
     const ele = document.getElementById("section-main");
     ele.removeAttribute("hidden");
+    ele.children[3].addEventListener("click", function() {
+        location.assign(`./?url=${ele.children[2].children[1].value}`);
+    });
 }
