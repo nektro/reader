973d177a08546a929cca9f98b171a7f3a9f1b53e
Meghan <nektromc@gmail.com>
2018-11-17 03:57:12 -0500

    add support for www.eater.com, closes #7

--- script.js
+++ script.js
@@ -41,6 +41,7 @@ function load_article(ele, url, html) {
         case "www.theverge.com":
         case "www.vox.com":
         case "www.sbnation.com":
+        case "www.eater.com":
         return Promise.resolve().then(() => {
             ele.appendChild(r_a_a(html.querySelector(".c-page-title")));
             ele.appendChild(r_a_a(html.querySelector(".c-entry-summary")));
