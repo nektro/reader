3f8b88ed31414eff9f879bf35e7665052e3cfa5a
Meghan <nektromc@gmail.com>
2018-11-17 05:23:00 -0500

    add support for 7500toholte.sbnation.com, closes #12

--- script.js
+++ script.js
@@ -41,6 +41,7 @@ function load_article(ele, url, html) {
         case "www.recode.net":
         case "www.theringer.com":
         case "product.voxmedia.com":
+        case "7500toholte.sbnation.com":
         return Promise.resolve().then(() => {
             ele.appendChild(r_a_a(html.querySelector(".c-page-title")));
             ele.appendChild(r_a_a(html.querySelector(".c-entry-summary")));
